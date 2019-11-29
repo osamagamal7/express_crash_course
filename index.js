48a0ea79588fd13dfc5c083f8eeaf01c708cb84e
osamagamal7 <31896040+osamagamal7@users.noreply.github.com>
2019-11-29 15:26:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 
